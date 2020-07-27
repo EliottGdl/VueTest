78f6bf1c02f1a8a1a71eb2806725a2e78aff81ea
Eliott Gandiolle <eliottgdl@gmail.com>
2020-07-27 13:45:37 +0200

    first test on vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store.js";
 
 Vue.use(VueRouter);
 
@@ -8,7 +9,8 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    props: true,
   },
   {
     path: "/about",
@@ -17,12 +19,116 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    props: true,
+  },
+  {
+    path: "/login",
+    name: "login",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+    props: true,
+  },
+  {
+    path: "/user",
+    name: "user",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "user" */ "../views/User.vue"),
+    props: true,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/invoices",
+    name: "invoices",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "invoices" */ "../views/Invoices.vue"),
+    props: true,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/destination/:slug",
+    name: "DestinationDetails",
+    component: () =>
+      import(
+        /* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails.vue"
+      ),
+    props: true,
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        component: () =>
+          import(
+            /* webpackChunkName: "DestinationDetails" */ "../views/ExperienceDetails.vue"
+          ),
+        props: true,
+      },
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        (destination) => destination.slug === to.params.slug
+      );
+      if (exists) {
+        next();
+      } else {
+        next({ name: "notFound" });
+      }
+    },
+  },
+  {
+    path: "/404",
+    alias: "*",
+    name: "notFound",
+    component: () =>
+      import(/* webpackChunkName: "NotFound" */ "../views/NotFound.vue"),
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: "history",
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      const position = {};
+      if (to.hash) {
+        position.selector = to.hash;
+        if (to.hash === "#experience") {
+          position.offset = { y: 50 };
+        }
+
+        if (document.querySelector(to.hash)) {
+          return position;
+        }
+        return false;
+      }
+    }
+  },
+  linkExactActiveClass: "jsuisActifBg",
+});
+
+router.beforeEach((to, from, next) => {
+  //if(to.meta.requiresAuth) {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!store.user) {
+      next({
+        name: "login",
+        query : {redirect : to.fullPath}
+      });
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1> All Destinations </h1>
+    <div class="destinations">
+    <div v-for="destination in destinations" :key="destination.name">
+      <router-link :to="{name : 'DestinationDetails',params:{slug:destination.slug}}">
+        <h2>{{destination.name}}</h2>
+      </router-link>
+      <figure>
+        <router-link :to="{name : 'DestinationDetails',params:{slug:destination.slug}}">
+          <img class="shake-slow" :src="require(`@/assets/${destination.image}`)" :alt="destination.name">
+        </router-link>
+      </figure>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import store from "@/store.js";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return { destinations: store.destinations };
   }
 };
 </script>
+
+<style scoped>
+
+@import '../assets/styles/csshake-slow.min.css';
+
+.home{
+  max-width:1600px;
+  margin : 0 auto;
+}
+
+img {
+  max-width:300px;
+}
+
+.destinations {
+  display:flex;
+  align-items: center;
+  justify-content:center;
+  flex-wrap: wrap;
+}
+
+a {
+  color : cornflowerblue;
+  text-decoration: none;
+  font-size:20px;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+a:visited {
+  text-decoration: underline;
+}
+
+</style>
